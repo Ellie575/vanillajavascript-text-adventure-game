162f136bde85815e5342c558d78a2c82eecb6b87
Ellie575 <elliekubo@hotmail.com>
2022-08-24 14:19:20 +1000

    Added next stages of javascript

--- script.js
+++ script.js
@@ -26,10 +26,17 @@ function showTextNode(textNodeIndex) {
 }
 
 function showOption(option) {
-  return true;
+  return option.requiredState == null || option.requiredState(state);
 }
 
-function selectOption(option) {}
+function selectOption(option) {
+  const nextTextNodeId = option.nextText;
+  if (nextTextNodeId <= 0) {
+    return startGame();
+  }
+  state = Object.assign(state, option.setState);
+  showTextNode(nextTextNodeId);
+}
 
 const textNodes = [
   {
@@ -39,7 +46,7 @@ const textNodes = [
       {
         text: "Take the goo",
         setState: { pinkGoo: true },
-        nextState: 2,
+        nextText: 2,
       },
       {
         text: "Leave the goo",
@@ -49,6 +56,73 @@ const textNodes = [
   },
   {
     id: 2,
+    text: "You venture forth in search of answers and you come across a merchant.",
+    options: [
+      {
+        text: "Trade the goo for fairy dust",
+        requiredState: (currentState) => currentState.pinkGoo,
+        setState: { pinkGoo: false, fairyDust: true },
+        nextText: 3,
+      },
+      {
+        text: "Trade the goo for a magic wand",
+        requiredState: (currentState) => currentState.pinkGoo,
+        setState: { pinkGoo: false, magicWand: true },
+        nextText: 3,
+      },
+      {
+        text: "Ignore the merchant.",
+        nextText: 3,
+      },
+    ],
+  },
+  {
+    id: 3,
+    text: "After seeing the merchant you feel tired and you stumble across a castle which looks like it's out of a fairytale.",
+    options: [
+      {
+        text: "Explore the castle",
+        nextText: 4,
+      },
+      {
+        text: "Knock on the gates",
+        nextText: 5,
+      },
+      {
+        text: "Find a hay stack to sleep on",
+        nextText: 6,
+      },
+    ],
+  },
+  {
+    id: 4,
+    text: "You are tired when you're inside the castle and the fairy Queen finds you asleep in the Hallway. She turns you into a frog for tresspassing the castle.",
+    options: [
+      {
+        text: "Restart",
+        nextText: -1,
+      },
+    ],
+  },
+  {
+    id: 5,
+    text: "The fairy Queen welcomes you into her castle and gives you food to eat and a bed to rest in.",
+    options: [
+      {
+        text: "Restart",
+        nextText: -1,
+      },
+    ],
+  },
+  {
+    id: 6,
+    text: "The castle servants finds you on the haystack and takes you into their quarters for food and rest until you are able to journey forth.",
+    options: [
+      {
+        text: "Restart",
+        nextText: -1,
+      },
+    ],
   },
 ];
 
